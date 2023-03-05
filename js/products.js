e04da725732f251b3044033170a632f298f27c28
LuzZummer <luzzummr@gmail.com>
2023-03-05 17:07:03 -0300

    first commit

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "shampoo", 
+    precio: 500,
+    img:
+    "https://production.na01.natura.com/on/demandware.static/-/Sites-natura-ar-storefront-catalog/default/dw9d6df530/NATARG-48099_1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "acondiconador", 
+    precio: 500,
+    img:
+    "https://production.na01.natura.com/on/demandware.static/-/Sites-natura-ar-storefront-catalog/default/dwab1b2a86/NATARG-48098_1.jpg",
+    cantidad: 1,
+  },
+  {
+    id:3,
+    nombre: "crema corporal", 
+    precio: 800,
+    img:
+    "https://production.na01.natura.com/on/demandware.static/-/Sites-natura-ar-storefront-catalog/default/dwbbd4122c/NATARG-94072_1.jpg",
+    cantidad: 1,
+  },
+  {
+    id:4,
+    nombre:"crema facial",
+    precio: 1000,
+    img:
+    "https://production.na01.natura.com/on/demandware.static/-/Sites-natura-ar-storefront-catalog/default/dw2ecc11bb/NATARG-59885_1.jpg",
+    cantidad: 1,
+  },
+];
